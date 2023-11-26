a17ced6ee871906bcbff9b93c8a23d480679ba2d
juthomas <juthomas@student.42.fr>
2023-11-26 19:08:17 +0100

    [TEST] Spi IO

--- desktop/sources/scripts/core/io/spi.js
+++ desktop/sources/scripts/core/io/spi.js
@@ -1,8 +1,12 @@
-'use strict'
+"use strict";
 
-function Spi (client) {
-	const spi = require('spi-device');
-	this.start - function () {
-		console.log('SPI', 'Starting..')
-	}
+function Spi(client) {
+  const spi = require("spi-device");
+  this.start -
+    function () {
+      console.log("SPI", "Starting..");
+    };
+  this.clear = function () {
+    // this.stack = []
+  };
 }
